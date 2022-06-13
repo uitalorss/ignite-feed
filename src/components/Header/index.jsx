b95d234368f31a85b608cd247924aca32d3178f3
Uítalo <uitalo.urss@gmail.com>
2022-06-12 23:43:40 -0300

    estilização do header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,10 @@
-import styles from './Header.module.css'
+import styles from './Header.module.css';
+import logoIgnite from '../../assets/ignite-simbol.svg';
 
 export function Header(){
   return(
     <header className={styles.header}>
+      <img src={logoIgnite} alt="Logo do Ignite" />
       <strong className={styles.header}>Ignite Feed</strong>
     </header>
   )
