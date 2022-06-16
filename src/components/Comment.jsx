4271a309c3528dea46db3b49caa92448f994da71
Uítalo <uitalo.urss@gmail.com>
2022-06-16 20:41:49 -0300

    estilização dos comentários do post

---
+++ src/components/Comment.jsx
@@ -0,0 +1,30 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+
+export function Comment(){
+  return(
+    <div className={styles.comment}>
+      <img src="https://github.com/uitalorss.png" alt="" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.infoAuthor}>
+              <strong>Uítalo Souza</strong>
+              <time title='14 de Junho de 2022 às 22:40' dateTime='2022-06-14 22:40:30'>Cerca de 20 minutos atrás</time>
+            </div>
+            <button className={styles.buttonDelete} title='deleteComment'>
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>Esse é um teste.</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp /> Aplaudir <span>2</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
